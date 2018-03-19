0ffa00b942cea3ccfb51a0c96111fd36e99acff3
Romain SIMON <contact@romainsimon.net>
2018-03-19 13:27:53 +0100

    Fix: Shortened code

--- life.js
+++ life.js
@@ -6,19 +6,7 @@ const lifeChance = process.argv[2] || Math.random();
 if (lifeChance < 0 || lifeChance > 1)
   throw new Error('Life chance must be between 0 and 1');
 let grid = [];
-
-// Draw a grid with initial life presence
-const initGrid = () => {
-  grid = [];
-  for (let i=0; i<h; i++) {
-    grid.push([]);
-    for (let j=0; j<w-1; j++) {
-      let isAlive = Math.random() > 1-lifeChance;
-      grid[i].push(isAlive);
-    }
-  }
-  displayGrid();
-}
+let init;
 
 // Display the grid in terminal
 const displayGrid = () => {
@@ -51,12 +39,15 @@ const evolveGrid = () => {
   newGrid = [];
   for (let i=0; i<h; i++) {
     newGrid.push([]);
-    for (let j=0; j<w-1; j++)
-      newGrid[i].push(isAlive(i, j));
+    for (let j=0; j<w-1; j++) {
+      let alive = init ? isAlive(i, j)
+        : Math.random() > 1-lifeChance;
+      newGrid[i].push(alive);
+    }
   }
   grid = newGrid;
+  init = true;
   displayGrid();
 }
 
-initGrid();
 setInterval(() => evolveGrid(), 100);
